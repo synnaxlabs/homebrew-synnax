578df6f5c330a140971fa5baa20ffa2c180e7510
MatchaOnMuffins <103859142+MatchaOnMuffins@users.noreply.github.com>
2023-09-18 21:16:36 +0000

    synnax: add 0.8.1 bottle.

--- Formula/synnax.rb
+++ Formula/synnax.rb
@@ -5,6 +5,13 @@ class Synnax < Formula
   sha256 "a8c443b501cfc0a028f18a7b7cc5d0edb03c46642ab0f158e8ff3d1adfa0ff75"
   license "BUSL-1.1"
 
+  bottle do
+    root_url "https://github.com/synnaxlabs/homebrew-synnax/releases/download/synnax-0.8.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:      "d02975c3c234ae8fcda24e95b4544cad851bf7cf3a3e9b14453b1a0e7ad7b07a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "07b77aaa176f4668084bf33986caa439aff7b62116755f7336380932605ef099"
+  end
+
   depends_on "go" => :build
 
   def install
