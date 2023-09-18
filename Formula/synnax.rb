281f8e3127c72cd99a37d9deaae770da3eaae3c5
Henry Zhang <henry@superfocus.ai>
2023-09-18 13:35:27 -0400

    Add synnax formula

---
+++ Formula/synnax.rb
@@ -0,0 +1,19 @@
+class Synnax < Formula
+  desc "Operate large scale hardware systems with ease"
+  homepage "https://github.com/synnaxlabs/synnax"
+  url "https://github.com/synnaxlabs/synnax/archive/refs/tags/synnax/v0.8.1.tar.gz"
+  sha256 "a8c443b501cfc0a028f18a7b7cc5d0edb03c46642ab0f158e8ff3d1adfa0ff75"
+  license "BUSL-1.1"
+
+  depends_on "go" => :build
+
+  def install # , *std_go_args(ldflags: "-s -w"), 
+    system "mkdir -p #{buildpath}/bin"
+    system "go", "build", "-o=./bin/synnax",  "./synnax"
+    bin.install "./bin/synnax"
+  end
+
+  test do
+    system "false"
+  end
+end
